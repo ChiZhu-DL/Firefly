36044abbfdb69d71bee14cf0e1f48c1f7b6167dc
ChiZhu <1843498737@qq.com>
2026-09-11 11:56:35 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,9 +67,9 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site_url,
+	site: "https://ChiZhu-DL.github.io",
 
-	base: "/",
+	base: "/Firefly/",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建
